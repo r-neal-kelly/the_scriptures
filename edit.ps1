a1033d659db1dc2fd79a2bb44b8cce5204628533
r-neal-kelly <r.neal.kelly@gmail.com>
2024-08-13 23:39:05 -0500

    now the edit and main scripts check for the needed command in PATH. also rebuilt using the newest versions on the tool chain

--- edit.ps1
+++ edit.ps1
@@ -1,1 +1,7 @@
+if ((Get-Command "ws" -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Host
+    Write-Host "This program requires local-web-server to be installed and available in the PATH."
+    Write-Host
+}
+
 ws --spa editor.html -p 8000
--- main.ps1
+++ main.ps1
@@ -1,1 +1,7 @@
+if ((Get-Command "ws" -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Host
+    Write-Host "This program requires local-web-server to be installed and available in the PATH."
+    Write-Host
+}
+
 ws --spa index.html -p 9000
